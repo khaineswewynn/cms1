2c0d96b4694e933945a209efdc00b3ad6922ed2d
khaineswewynn <khaineshwewynn@gmail.com>
2024-10-19 14:16:50 +0630

    added by developer1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -215,6 +215,7 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard By Dev 1</h1>
+        <h2>Updated By Deveoper 1</h2>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
     </div>
